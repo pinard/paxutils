7c9cc210270c9ce86517c75e393296ac664a444f
François Pinard <pinard@iro.umontreal.ca>
2009-07-08 08:12:44 -0400

    index.rst deleted, now a Tomboy note

---
+++ web/notes/site.mk
@@ -0,0 +1,5 @@
+package_name = paxutils
+long_package_name = "Free pax utilities"
+rootdir = $(HOME)/entretien/paxutils/web/notes
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
--- web/site.mk
+++ web/site.mk
@@ -1,6 +1,6 @@
 package_name = paxutils
 long_package_name = "Free pax utilities"
-rootdir = $(HOME)/fp/paxutils/web
+rootdir = $(HOME)/entretien/paxutils/web
 margin_color = "\#cccccc"
 caption_color = "\#f0f0f0"
 
