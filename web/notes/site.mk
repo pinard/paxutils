2b6aac7d3e4fdda311fbcee9a83b8bdf574ae2d7
François Pinard <pinard@iro.umontreal.ca>
2009-07-18 13:42:35 -0400

    site.mk: Declare rootdir at top level

--- web/notes/site.mk
+++ web/notes/site.mk
@@ -1,5 +1,5 @@
 package_name = paxutils
 long_package_name = "Free pax utilities"
-rootdir = $(HOME)/entretien/paxutils/web/notes
+rootdir = $(HOME)/entretien/paxutils/web
 
 TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
